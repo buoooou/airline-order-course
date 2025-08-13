8581b9f8aa33b584319c7912d897017c29e90f31
Cai Rong Yue <cryue@cn.ibm.com>
2025-08-13 14:34:44 +0800

    update

--- backend/src/main/java/com/postion/airlineorderbackend/AirlineOrderBackendApplication.java
+++ backend/src/main/java/com/postion/airlineorderbackend/AirlineOrderBackendApplication.java
@@ -1,6 +1,11 @@
 package com.postion.airlineorderbackend;
 
+import java.util.List;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,4 +19,16 @@ public class AirlineOrderBackendApplication {
 		  SpringApplication.run(AirlineOrderBackendApplication.class, args);
     }
 
+	  @Bean
+	  public CorsFilter corsFilter() {
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowedOriginPatterns(List.of("*"));
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("*");
+	    config.setAllowCredentials(true);
+
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
+	  }
 }
