51a703d87c2fb42d77de6badd24eea9886ce2435
Wen Rong <rongwen@cn.ibm.com>
2025-08-18 15:56:03 +0800

    fix: update frontend bug

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 // src/app/app.routes.ts
 
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login';
-import { OrderListComponent } from './pages/order-list/order-list';
-import { OrderDetailComponent } from './pages/order-detail/order-detail';
-import { authGuard } from './core/guards/auth-guard';
+import { LoginComponent } from './pages/login/login.component';
+import { OrderListComponent } from './pages/order-list/order-list.component';
+import { OrderDetailComponent } from './pages/order-detail/order-detail.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
