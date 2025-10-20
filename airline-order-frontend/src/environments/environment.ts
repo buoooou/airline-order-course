feac9d157e1e14bed1faa0d2a601377e16b05038
gina9500 <48407032+gina9500@users.noreply.github.com>
2025-10-20 14:19:23 +0800

    Update environment.ts

--- airline-order-frontend/src/environments/environment.ts
+++ airline-order-frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   // 在本地开发时，apiUrl 可以是空字符串，因为我们会用代理。
   // 或者你也可以写上完整的后端地址，但代理是更好的选择。
-  apiUrl: 'http://18.191.37.120:8081' 
+  apiUrl: 'http://13.62.76.189:8081' 
 };
